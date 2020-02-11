4a3f9a06f8a8425aa2c9ca796477b5f961458997
Roman Dvořák <romandvorak@mlab.cz>
2020-02-11 15:24:35 +0100

    add setup.py script

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(
+    name = 'yagv',
+    version = '0.4',
+    package_dir = {"":'.'},
+    description = '',
+    packages = setuptools.find_packages(),
+    classifiers = [
+
+    ],
+    scripts = ['yagv'],
+    #python_requies='>3.6'
+)
