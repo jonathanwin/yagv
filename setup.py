6e13567cdcf060def1e0d2ea61ec5026a71a7cd9
Roman Dvořák <romandvorak@mlab.cz>
2020-03-03 21:44:35 +0100

    Uprava struktury repozitare

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@ import setuptools
 
 
 setuptools.setup(
-    name = 'yagvTF',
+    name = 'yagv',
     version = '0.4',
-    package_dir = {"":'.'},
     description = '',
     packages = setuptools.find_packages(),
+    package_dir = {"":'.'},
+    data_files=[
+        ("data", ("data/hana_swimsuit_fv_solid_v1.gcode",))
+    ],
     classifiers = [
 
     ],
+    #include_package_data = True,
+    #py_modules = ['gcodeParser'],
     scripts = ['yagv'],
-    #python_requies='>3.6'
+    python_requies='>3.6'
 )
