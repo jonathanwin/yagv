4e6ecb91f19d9e4bc9c7df02792e9f352c4dbd8c
Rene K. Mueller <spiritdude@gmail.com>
2021-04-12 11:20:18 +0200

    0.5.1: adding bed grid

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = 'yagv',
-    version = '0.5.0',
+    version = '0.5.1',
     description = 'Yet another Gcode viewer (3D Printing Format)',
     packages = setuptools.find_packages(),
     package_dir = {"":'.'},
